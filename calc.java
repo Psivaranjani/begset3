ebc3ddbbfecb0ef827df0127ad544a674f7f0275
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-08-18 14:42:57 +0530

    Create calc.java

---
+++ calc.java
@@ -0,0 +1,23 @@
+# begset3
+import java.util.Scanner;
+public class calc {
+public static void main(String args[]) 
+{
+Scanner s=new Scanner(System.in);
+Integer a=s.nextInt();
+System.out.println("Enter number:"+a);
+if(a==0)
+{
+	System.out.println(1);
+}else
+{
+int b=1;
+for(int i=1;i<=a;i++)
+{
+	b=b*i;
+}
+
+System.out.println(b);
+}
+}
+}
